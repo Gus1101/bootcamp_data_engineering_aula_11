c7716dcf4847682ac4b71b20d08c7251f7971b27
Gus1101 <augustosantosb60@gmail.com>
2024-06-14 18:04:45 -0300

    adição do arquivo com classe para processamento de CSV

---
+++ teste_classe.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+class CsvProcessor:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.df = None
+
+    def read_archive(self):
+        self.df = pd.read_csv(self.file_path)
+
+    def filter_archive(self, column: str, parameter: list):
+        return self.df[self.df[column] == parameter]
+        
+
+path = './arquivo_teste.csv'
+coluna = 'nome'
+parametro = 'Produto A'
+
+arquivo_csv = CsvProcessor(path)
+arquivo_csv.read_archive()
+print(arquivo_csv.filter_archive(coluna, parametro))
